a1db593a5eddde69438fe2d235a78d4a82959c96
David <dje4001@med.cornell.edu>
2022-06-10 16:21:52 -0400

    test

---
+++ ZipSync.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#Loop through IPs
+IPs=( pi@ )
+t_pwd="raspberry"
+
+
+
+#Go to the ziplist folder directory
+#Open zip list
+#loop through list and zip video files and put in new folder
+
+
+#rsync all zipped data to the primary drive on conductor raspberry pi
+#delete zip folder
+#move zip list to zip list archive
+
+#exit
+
+
