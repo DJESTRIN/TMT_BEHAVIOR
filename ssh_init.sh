232cee202290ddc7d9855d135de4cf951029f9fc
David <david.james.estrin@outlook.com>
2022-06-16 14:35:17 -0400

    corrected the for loop

--- ssh_init.sh
+++ ssh_init.sh
@@ -4,5 +4,5 @@ Lines=$(cat IPs.txt)
 for line in $Lines;
 do
 echo ${line[0]}
-sshpass -p $t_pwd ssh -t ${line[0]} 'echo "Success" && exit; exec bash -l' && echo "${boxs[@]}"
+sshpass -p "raspberry" ssh -tt ${line[0]} 'echo "Success" && exit; exec bash -l'
 done
