af1cd24a5844b37a622b66f74164420dc9ef9e52
David <david.james.estrin@outlook.com>
2022-06-16 14:33:06 -0400

    corrected the for loop

--- ssh_init.sh
+++ ssh_init.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
-
-IPs=( pi@169.254.177.61 pi@192.168.0.246 pi@192.168.0.169 pi@192.168.0.227\
- pi@192.168.0.108 pi@192.168.0.103 pi192.168.0.249 pi@192.168.0.224 pi@192.168.0.172\
- pi@192.168.0.129 pi@192.168.0.185 pi@192.168.0.122 pi@192.168.0.191 pi@192.168.0.217 )
-
-t_pwd="raspberry"
-
-boxs=( 6 5 9 10 11 14 15 12 13 7 4 1 3 2 )
-
-for IP in "${IPs[@]}"
+cd ~/TMT_BEHAVIOR/
+Lines=$(cat IPs.txt)
+for line in $Lines;
 do
-sshpass -p $t_pwd ssh -t $IP 'echo "Success" && exit; exec bash -l' && echo "${boxs[@]}"
+echo ${line[0]}
+sshpass -p $t_pwd ssh -t ${line[0]} 'echo "Success" && exit; exec bash -l' && echo "${boxs[@]}"
 done
