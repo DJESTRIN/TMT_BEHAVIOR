084fe51de2cd9a26ec356bcf72c129210de0b436
David <david.james.estrin@outlook.com>
2022-07-01 12:45:18 -0400

    updated preview.py again

--- TestCamera.sh
+++ TestCamera.sh
@@ -4,6 +4,6 @@ cd ~/TMT_BEHAVIOR/
 while IFS=, read IP box; do
 echo "$IP"
 echo This is box "$box"
-sshpass -p "estrin1" ssh -n -tt $IP 'python ~/base/code/temp/preview.py && rsync -var ~/test_zip.zip pi@192.168.0.147:~/video_archive/$box/ && exit; exec bash -l'
+sshpass -p "estrin1" ssh -n -tt $IP 'sudo python ~/base/code/temp/preview.py && rsync -var ~/test_zip.zip pi@192.168.0.147:~/video_archive/$box/ && exit; exec bash -l'
 done < IPS.txt
 
--- preview.py
+++ preview.py
@@ -4,13 +4,13 @@ import os
 import time
 
 filename = str(time.time())+"_test.h264"
-directory = '~/base/videos/'
+directory = "/home/pi/base/videos/"
 camera = picamera.PiCamera()
 os.chdir(directory)
 camera.start_recording(filename)
 time.sleep(10)
 camera.stop_recording()
 
-os.chdir("~/")
+os.chdir("/home/pi/")
 with ZipFile("test_zip.zip", 'w') as zipf:
     zipf.write(os.path.join(directory,filename), arcname=filename)
