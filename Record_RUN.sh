a707eb3b325aeaac8aafd3a4400c38fd8ce1d86a
David <david.james.estrin@outlook.com>
2022-07-11 18:48:05 -0400

    updated record_run IP

--- Record_RUN.sh
+++ Record_RUN.sh
@@ -11,7 +11,7 @@ echo $IP
 
 if [ $IP == $IPaddress ];
 	then
-		sudo python ~/base/code/temp/Record.py --IP $IP --box $box --experiment $experiment --cage $cage --animal $animal --totaltime $totaltime --sex $sex --weight $weight --dob $dob --strain $strain --virus $virus --day $day --notes $notes --ziplistfile $ziplistfile --videodir $videodir --logdir $logdir
+		sudo python ~/base/code/temp/Record.py --ip $IP --box $box --experiment $experiment --cage $cage --animal $animal --totaltime $totaltime --sex $sex --weight $weight --dob $dob --strain $strain --virus $virus --day $day --notes $notes --ziplistfile $ziplistfile --videodir $videodir --logdir $logdir
 		break
 else
 	echo "Addresses do not match"
