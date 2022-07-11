b061f7099476c69ba9140f3b5c12f685153dc265
David <david.james.estrin@outlook.com>
2022-07-11 13:52:41 -0400

    create a delete videos script

--- Record_RUN.sh
+++ Record_RUN.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 echo "Recording video"
-sudo python ~/base/code/temp/Record.py --IP $IP --box $box --experiment $experiment --cage $cage --animal $animal --totaltime $totaltime --sex $sex --weight $weight --dob $dob --strain $strain --virus $virus --day $day --notes $notes --ziplistfile $ziplistfile --videodir $video_dir --logdir logdir
+while IFS=, read IP box experiment cage animal totaltime sex weight dob strain virus day notes ziplistfile videodir logdir;
+do
+IPaddress=$(hostname -I)
+IPaddress="$IPaddress"
+IPaddress="pi@"$IPaddress
+
+if [["$IP"== "$IPaddress"]];
+	then
+		sudo python ~/base/code/temp/Record.py --IP $IP --box $box --experiment $experiment --cage $cage --animal $animal --totaltime $totaltime --sex $sex --weight $weight --dob $dob --strain $strain --virus $virus --day $day --notes $notes --ziplistfile $ziplistfile --videodir $videodir --logdir $logdir
+		break
+else
+	echo "Addresses do not match"
+fi
+done < ~/base/code/temp/schedule.txt
+
 exit
 exec $SHELL
---
+++ delete_videos.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#All targets in orchestra
+cd ~/TMT_BEHAVIOR/
+Lines=$(cat IPs.txt)
+
+#Loop through IP addresses
+for IP in $Lines;
+do
+#Delete all videos on the pi
+sshpass -p "estrin1" ssh -tt ${IP[0]} 'sudo rm -rf /home/pi/base/videos/* && exit; exec bash -l'
+done
+
+
