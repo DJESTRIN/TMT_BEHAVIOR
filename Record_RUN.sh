7d516ee26f62e146946b52e8b01d5c1a7bcc1c80
DJESTRIN <53871591+DJESTRIN@users.noreply.github.com>
2022-07-14 11:55:41 -0400

    Update Record_RUN.sh

--- Record_RUN.sh
+++ Record_RUN.sh
@@ -14,8 +14,8 @@ if [ $IP == $IPaddress ];
 		todaydir="$(date +"%m_%d_%Y")"
 		videodir="$videodir$todaydir/"
 		logdir="$logdir$todaydir/"
-		mkdir -p videodir
-		mkdir -p logdir
+		mkdir -p $videodir
+		mkdir -p $logdir
 		echo "IP addresses MATCH!"
 		sudo python /home/pi/base/code/temp/Record.py --ip $IP --box $box --experiment $experiment --cage $cage --animal $animal --totaltime $totaltime --sex $sex --weight $weight --dob $dob --strain $strain --virus $virus --day $day --notes $notes --ziplistfile $ziplistfile --videodir $videodir --logdir $logdir
 		break
