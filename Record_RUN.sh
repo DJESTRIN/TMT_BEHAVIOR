8802a87ac6bfaa9a9a2cdd250de1d182d2264494
DJESTRIN <53871591+DJESTRIN@users.noreply.github.com>
2022-07-14 11:06:00 -0400

    updated directories to create a date folder

--- Record_RUN.sh
+++ Record_RUN.sh
@@ -11,6 +11,9 @@ echo $IP
 
 if [ $IP == $IPaddress ];
 	then
+		todaydir="$(date +"%m_%d_%Y")"
+		videodir="$videodir/$todaydir/"
+		logdir="$logdir/$todaydir/"
 		echo "IP addresses MATCH!"
 		sudo python /media/usb/base/code/temp/Record.py --ip $IP --box $box --experiment $experiment --cage $cage --animal $animal --totaltime $totaltime --sex $sex --weight $weight --dob $dob --strain $strain --virus $virus --day $day --notes $notes --ziplistfile $ziplistfile --videodir $videodir --logdir $logdir
 		break
