c0e3c3b8432df00245ad98fcedef759c000cbbd6
David <david.james.estrin@outlook.com>
2022-07-22 17:39:14 -0400

    create mount code

---
+++ mountdrives.sh
@@ -0,0 +1,7 @@
+#!/bin/bash/
+cd ~/TMT_BEHAVIOR/
+Lines=$(cat IPs.txt)
+for IP in $Lines;
+do
+sshpass -p "estrin1" ssh -t $IP 'sudo mkdir -p /media/usb/ && sudo mount /dev/sdb1 /media/usb -o uid=pi,gid=pi && exit; exec bash -;'
+done
