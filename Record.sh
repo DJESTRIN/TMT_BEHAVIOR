39f60a38bb0726fe1271cbcfae9415cb5fa7b8e7
David <david.james.estrin@outlook.com>
2022-07-26 16:55:12 -0400

    updated the record run to give sudo privilage

--- Record.sh
+++ Record.sh
@@ -6,5 +6,5 @@ echo $message
 
 while IFS=, read IP box experiment cage animal totaltime sex weight dob strain virus day notes ziplistfile videodir logdir;
 do
-sshpass -p "estrin1" ssh -n -tt $IP 'tag=$(date "+%Y%m%d%H%M%S") && sudo screen -dmS $tag bash -c "echo $message; exec bash" && sudo screen -r $tag -p 0 -X stuff "bash /home/pi/base/code/temp/Record_RUN.sh\n" && exit; exec bash -l'
+sshpass -p "estrin1" ssh -n -tt $IP 'tag=$(date "+%Y%m%d%H%M%S") && screen -dmS $tag bash -c "echo $message; exec bash" && screen -r $tag -p 0 -X stuff "bash /home/pi/base/code/temp/Record_RUN.sh\n" && exit; exec bash -l'
 done < schedule.txt
