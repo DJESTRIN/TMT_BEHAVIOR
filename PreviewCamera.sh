e4c017dcade73b0c7b0920705ff7a29708ec238a
DJESTRIN <53871591+DJESTRIN@users.noreply.github.com>
2022-12-11 12:20:43 -0500

    Add files via upload

--- PreviewCamera.sh
+++ PreviewCamera.sh
@@ -6,7 +6,7 @@ Lines=$(cat IPs.txt)
 for IP in $Lines
 do
 echo ${IP[0]}
-sshpass -p "estrin1" ssh -tt ${IP[0]} 'screen -dm bash -c "sudo python /media/usb/base/code/temp/preview.py" && exit; exec bash -l'
+sshpass -p "estrin1" ssh -tt ${IP[0]} 'screen -dm bash -c "python /media/usb/base/code/temp/preview.py" && exit; exec bash -l'
 done
 
 echo "Finished previewing cameras"
--- send_schedule.sh
+++ send_schedule.sh
@@ -3,6 +3,6 @@ cd ~/TMT_BEHAVIOR/
 Lines=$(cat IPs.txt)
 for IP in $Lines;
 do
-sshpass -p "estrin1" ssh -t $IP 'sudo mkdir -p /media/usb/base/schedule/ && exit; exec bash -;'
-/usr/bin/rsync -var --rsh="/usr/bin/sshpass -p estrin1 ssh -o StrictHostKeyChecking=no -l pi" ~/Desktop/schedule $IP:/media/usb/base/schedule
+sshpass -p "estrin1" ssh -t $IP 'sudo mkdir -p /home/pi/Desktop/schedule/ && exit; exec bash -;'
+/usr/bin/rsync -var --rsh="/usr/bin/sshpass -p estrin1 ssh -o StrictHostKeyChecking=no -l pi" ~/Desktop/schedule/* $IP:/home/pi/Desktop/schedule/
 done
