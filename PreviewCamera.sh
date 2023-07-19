2b3bb27652048467feb37fe525ab6c3ed02c121c
DJESTRIN <53871591+DJESTRIN@users.noreply.github.com>
2023-07-19 12:33:44 -0400

    Update PreviewCamera.sh

--- PreviewCamera.sh
+++ PreviewCamera.sh
@@ -6,7 +6,7 @@ Lines=$(cat IPs.txt)
 for IP in $Lines
 do
 echo ${IP[0]}
-sshpass -p "estrin1" ssh -tt ${IP[0]} 'screen -dm bash -c "python /media/usb/base/code/temp/preview.py" && exit; exec bash -l'
+sshpass -p "estrin1" ssh -tt ${IP[0]} 'screen -dm bash -c "python /home/pi/TMT_BEHAVIOR/preview.py" && exit; exec bash -l'
 done
 
 echo "Finished previewing cameras"
