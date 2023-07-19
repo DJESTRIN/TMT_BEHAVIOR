e7708317900fc3aaf897c2a10e913e3b8558abff
DJESTRIN <53871591+DJESTRIN@users.noreply.github.com>
2023-07-19 12:41:13 -0400

    Update Record_RUN.sh

--- Record_RUN.sh
+++ Record_RUN.sh
@@ -17,12 +17,12 @@ if [ $IP == $IPaddress ];
 		sudo mkdir -p $videodir
 		sudo mkdir -p $logdir
 		echo "IP addresses MATCH!"
-		sudo python /media/usb/base/code/temp/Record.py --ip $IP --box $box --experiment $experiment --cage $cage --animal $animal --totaltime $totaltime --sex $sex --weight $weight --dob $dob --strain $strain --virus $virus --day $day --notes $notes --ziplistfile $ziplistfile --videodir $videodir --logdir $logdir
+		sudo python /home/pi/TMT_BEHAVIOR/Record.py --ip $IP --box $box --experiment $experiment --cage $cage --animal $animal --totaltime $totaltime --sex $sex --weight $weight --dob $dob --strain $strain --virus $virus --day $day --notes $notes --ziplistfile $ziplistfile --videodir $videodir --logdir $logdir
 		break
 else
 	echo "Addresses do not match"
 fi
-done < /media/usb/base/code/temp/schedule.txt
+done < /home/pi/schedule.txt
 
 exit
 exec $SHELL
