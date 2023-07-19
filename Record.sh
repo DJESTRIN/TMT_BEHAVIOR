38aac578f49ed9ea4f300a8d6b09e5f4d7afa771
DJESTRIN <53871591+DJESTRIN@users.noreply.github.com>
2023-07-19 12:37:37 -0400

    Update Record.sh

--- Record.sh
+++ Record.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #All targets in orchestra
-cd ~/TMT_BEHAVIOR/
+cd /home/pi/TMT_BEHAVIOR/
 message="Trying to initiate screen session"
 echo $message
 
 while IFS=, read IP box experiment cage animal totaltime sex weight dob strain virus day notes ziplistfile videodir logdir;
 do
-sshpass -p "estrin1" ssh -n -tt $IP 'tag=$(date "+%Y%m%d%H%M%S") && screen -dmS $tag bash -c "echo $message; exec bash" && screen -r $tag -p 0 -X stuff "bash /home/pi/base/code/temp/Record_RUN.sh\n" && exit; exec bash -l'
-done < /media/usb/base/code/temp/schedule.txt
+sshpass -p "estrin1" ssh -n -tt $IP 'tag=$(date "+%Y%m%d%H%M%S") && screen -dmS $tag bash -c "echo $message; exec bash" && screen -r $tag -p 0 -X stuff "bash /home/pi/TMT_BEHAVIOR/Record_RUN.sh\n" && exit; exec bash -l'
+done < /home/pi/schedule.txt
