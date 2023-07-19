62232578504f131b3c6b4f6f361ae7a0feade248
David Estrin <david.james.estrin@outlook.com>
2023-07-19 16:16:15 -0400

    Updated NAS branch

--- config_pis.sh
+++ config_pis.sh
@@ -1,36 +1,37 @@
 #!/bin/bash
 echo on
 
-#Delete folders of interest
-cd
-sudo rm -rf temp 
-sudo rm -rf base
-
-#All targets in orchestra
-cd ~/TMT_BEHAVIOR/
-Lines=$(cat IPs.txt)
+# Go to code repo on main raspberry pi
+cd /home/pi/TMT_BEHAVIOR/
+git checkout NAS
 
 # SSH in and prepare the PIs
-# Set up folders
-base_dirs=( /media/usb/base/logs/ /media/usb/base/videos/ /media/usb/base/code/ )
-code_dir=( /media/usb/base/code/temp/)
+# Set up mounted drive folder
+data_drop=( /home/pi/NAS/ )
 
 #Loop through IP addresses
-for IP in $Lines;
+while IFS=, read IP box;
 do
+	#Create mounted directory variable
+	mounted_directory=/share/data/bb0046/box$box
+	
+	# Generate path for network drive
+	sshpass -p "estrin1" ssh -t ${IP[0]} 'sudo mkdir -p '$data_drop' && sudo chmod -R 777 '$data_drop' && sudo chown pi:pi '$data_drop' && exit; exec bash -l'
+	
+	#install packages
+	sshpass -p "estrin1" ssh -t ${IP[0]} '(sudo apt-get install sshpass nfs-common portmap screen || echo 'instalation did not work') && exit; exec bash -l'
+	
+	#Kill all screen sessions
+	sshpass -p "estrin1" ssh -t ${IP[0]} '(sudo killall screen || echo nvm) && exit; exec bash -l'
+	
+	#Mount the NAS virtual directory
+	sshpass -p "estrin1" ssh -t ${IP[0]} 'sudo service rpcbind start && sudo update-rc.d rpcbind enable && showmount -e 192.168.6.1 && sudo mount -t nfs 
+192.168.6.1:$mounted_directory '$d' && exit; exec bash -l'
 
-	#build base directories
-	for d in "${base_dirs[@]}";
-	do
-		echo $d
-		sshpass -p "estrin1" ssh -t ${IP[0]} 'sudo mkdir -p '$d' && sudo rm -rf ~/temp && sudo rm -rf '${code_dir[0]}' && exit; exec bash -l'
-	done
+	#git clone all nessesary code
+	sshpass -p "estrin1" ssh -tt ${IP[0]} 'cd /home/pi/ && git clone https://github.com/DJESTRIN/TMT_BEHAVIOR/' && exit; exec bash -l'
 
-#git clone all nessesary code
-sshpass -p "estrin1" ssh -t ${IP[0]} '(sudo killall screen || echo nvm) && exit; exec bash -l'
-sshpass -p "estrin1" ssh -tt ${IP[0]} 'git clone https://github.com/DJESTRIN/TMT_BEHAVIOR/ temp && sudo mv -v temp '${base_dirs[2]}' &&\
-sudo rm -rf temp && exit; exec bash -l'
-done
+done < /home/pi/TMT_BEHAVIOR/IPs.txt
 
 echo "Finished preparing this raspberry pi"
 echo off
