5e4c0d38ff3b56da2094f4c927670a36a01056ee
DJESTRIN <53871591+DJESTRIN@users.noreply.github.com>
2023-07-19 12:43:56 -0400

    Update TestCamera.sh

--- TestCamera.sh
+++ TestCamera.sh
@@ -2,15 +2,15 @@
 #All targets in orchestra
 cd ~/TMT_BEHAVIOR/
 Lines=$(cat IPs.txt)
-sudo rm -rf ~/test_video_archive/
-mkdir -p ~/test_video_archive/
+sudo rm -rf /home/pi/test_video_archive/
+mkdir -p /home/pi/test_video_archive/
 
 message="start up screen session"
 echo $message
 for line in $Lines;
 do
 echo ${line[0]}
-sshpass -p "estrin1" ssh -n -tt ${line[0]} 'tag=$(date "+%Y%m%d%H%M%S") && screen -dmS $tag bash -c "echo $message; exec bash" && screen -r $tag -p 0 -X stuff "bash ~/base/code/temp/RUN.sh\n" && exit; exec bash -l'
+sshpass -p "estrin1" ssh -n -tt ${line[0]} 'tag=$(date "+%Y%m%d%H%M%S") && screen -dmS $tag bash -c "echo $message; exec bash" && screen -r $tag -p 0 -X stuff "bash /home/pi/TMT_BEHAVIOR/RUN.sh\n" && exit; exec bash -l'
 done
 
 sleep 60
@@ -19,7 +19,7 @@ echo "Zipping videos"
 while IFS=, read IP box; do
 echo "Zipping videos for box" $box
 mkdir -p ~/test_video_archive/${box}/
-sshpass -p "estrin1" rsync -chavzP --stats $IP:~/base/videos/test_zip.zip ~/test_video_archive/$box/ || echo "Error occured for " $box
+sshpass -p "estrin1" rsync -chavzP --stats $IP:/home/pi/NAS/test_zip.zip /home/pi/test_video_archive/$box/ || echo "Error occured for " $box
 done < IPS.txt
 
 while IFS=, read IP box; do
