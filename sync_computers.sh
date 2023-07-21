dcced2832e69cf61cf0da68d01baa219a82503c1
DJESTRIN <53871591+DJESTRIN@users.noreply.github.com>
2023-07-21 14:51:49 -0400

    Create sync_computers.sh

---
+++ sync_computers.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Syncrhonising computers"
+while IFS=, read IP box;
+do
+
+echo $IP
+rsync -var /home/pi/TMT_BEHAVIOR/ $IP:/home/pi/TMT_BEHAVIOR/ 
+
+done < /home/pi/TMT_BEHAVIOR/IPSaddresses.txt
+
+exit
+exec $SHELL
